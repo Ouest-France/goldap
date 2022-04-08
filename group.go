77dc87246701241effd1c0dca9c890e6abe00060
Pablo RUTH <contact@pablo-ruth.fr>
2022-04-08 10:40:05 +0200

    add paging to member attribute on group read

--- group.go
+++ group.go
@@ -1,6 +1,8 @@
 package goldap
 
 import (
+	"fmt"
+
 	"github.com/go-ldap/ldap/v3"
 )
 
@@ -23,8 +25,9 @@ func (c *Client) CreateGroup(dn, name string, description string, members []stri
 }
 
 // ReadGroup reads ldap group and return it's attributes on an error if the group donesn't exist
-func (c *Client) ReadGroup(dn string) (attributes map[string][]string, err error) {
+func (c *Client) ReadGroup(dn string, memberPageSize int) (attributes map[string][]string, err error) {
 
+	// Request name and description attributes
 	req := ldap.NewSearchRequest(
 		dn,
 		ldap.ScopeBaseObject,
@@ -33,7 +36,7 @@ func (c *Client) ReadGroup(dn string) (attributes map[string][]string, err error
 		0,
 		false,
 		"(objectclass=group)",
-		[]string{"*"},
+		[]string{"name", "description"},
 		[]ldap.Control{},
 	)
 
@@ -45,8 +48,61 @@ func (c *Client) ReadGroup(dn string) (attributes map[string][]string, err error
 	attributes = map[string][]string{}
 	for _, entry := range sr.Entries {
 		for _, attr := range entry.Attributes {
+
 			attributes[attr.Name] = attr.Values
 		}
+	}
+
+	// Page member attribute
+	page := 0
+	attributes["member"] = []string{}
+	for {
+		fmt.Printf("member;range=%d-%d\n", memberPageSize*page, (memberPageSize-1)+memberPageSize*page)
+
+		// Request member attribute page
+		req := ldap.NewSearchRequest(
+			dn,
+			ldap.ScopeBaseObject,
+			ldap.NeverDerefAliases,
+			0,
+			0,
+			false,
+			"(objectclass=group)",
+			[]string{fmt.Sprintf("member;range=%d-%d", memberPageSize*page, (memberPageSize-1)+memberPageSize*page)},
+			[]ldap.Control{},
+		)
+
+		// Search
+		sr, err := c.Conn.Search(req)
+		if err != nil {
+			return attributes, err
+		}
+
+		// If no attributes found, member doesn't exists, break
+		if len(sr.Entries) == 0 {
+			break
+		}
+
+		// If more than one entry, it's an error
+		if len(sr.Entries) > 1 {
+			return attributes, fmt.Errorf("more than one entry found retrieving group members")
+		}
+
+		// If no attributes found, member doesn't exists, break
+		if len(sr.Entries[0].Attributes) == 0 {
+			break
+		}
+
+		// Append member attribute
+		attributes["member"] = append(attributes["member"], sr.Entries[0].Attributes[0].Values...)
+
+		// If no more pages, break
+		if len(sr.Entries[0].Attributes[0].Values) < memberPageSize {
+			break
+		}
+
+		// Next page
+		page++
 	}
 
 	return attributes, nil
