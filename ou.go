52927370beb8222f6e1c8b9e1d6d457cb8bf973c
Pablo RUTH <contact@pablo-ruth.fr>
2023-01-23 14:53:18 +0100

    Add SearchOUByName

--- ou.go
+++ ou.go
@@ -23,6 +23,42 @@ func (c *Client) CreateOrganizationalUnit(dn, description, managedBy string) err
 	return c.Conn.Add(req)
 }
 
+// SearchOUByName searches an LDAP OU by name and returns its DN
+func (c *Client) SearchOUByName(name, ou string, scope int) (string, error) {
+
+	// Request name and description attributes
+	req := ldap.NewSearchRequest(
+		ou,
+		scope,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		fmt.Sprintf("(&(objectClass=organizationalUnit)(ou=%s))", name),
+		[]string{},
+		[]ldap.Control{},
+	)
+
+	// Search for OU
+	sr, err := c.Conn.Search(req)
+	if err != nil {
+		return "", fmt.Errorf("searching OU by name: %s", err)
+	}
+
+	// If no entries found, OU doesn't exists, return error
+	if len(sr.Entries) == 0 {
+		return "", fmt.Errorf("OU %q not found in OU %q", name, ou)
+	}
+
+	// If more than one entry, it's an error
+	if len(sr.Entries) > 1 {
+		return "", fmt.Errorf("more than one OU found with name %q in OU %q", name, ou)
+	}
+
+	// Return OU DN
+	return sr.Entries[0].DN, nil
+}
+
 // ReadOrganizationalUnit reads an OrganizationalUnit
 func (c *Client) ReadOrganizationalUnit(dn string) (entries map[string][]string, err error) {
 
